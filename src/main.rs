54f49e14ca0b0583c5f1ace452f289a28eadab92
Matt Kelly <matthew.ryan.kelly@gmail.com>
2023-02-08 22:31:29 -0500

    Render barebones canvas with stock resources

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use rltk::{Rltk, GameState};
+
+struct State {}
+impl GameState for State {
+    fn tick(&mut self, ctx : &mut Rltk) {
+        ctx.cls();
+        ctx.print(1, 1, "Hullo, Singularity6!")
+    }
+}
+
+fn main() -> rltk::BError {
+    use rltk::RltkBuilder;
+    let context = RltkBuilder::simple80x50()
+        .with_title("A Roguelike Resume")
+        .build()?;
+    let gs = State{ };
+    rltk::main_loop(context, gs)
 }
