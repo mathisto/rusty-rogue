5d1cc1529dfe28776fd5e88e75a62de8dc06c0be
Matt Kelly <matthew.ryan.kelly@gmail.com>
2023-02-09 00:45:43 -0500

    Add keyboard movement via arrows and vim-like hjkl

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 I heard y'all are a Rust shop. I've toyed around before but never made anything of substance. Here is to learnin' a bit o' Rust with this deliciously polished [roguelike tutorial](https://bfnightly.bracketproductions.com/rustbook/chapter_1.html). This seemed too perfect an opportunity to learn how you witches and warlocks in gamedevland manage to do *so much* in 1/60th a second. Even more interesting, I've never used an ECS and it looks like a really interesting technique for managing complexity.
 
 I'll keep good commit hygeine so you can easily read the log for a high level overview of the process I went through. I'll make sure to preserve any mistakes I make in the commits along with the lesson learned.
+
+## Demo
+1. Clone the repo: `git clone https://github.com/mathisto/rusty-rogue.git`
+2. Run the game: `cargo run`
+
+## Notes:
+[Dwarf Fortress' ASCII Glyph Table](http://dwarffortresswiki.org/index.php/Character_table)
+
+### Chapter 2 (Complete)
+Whoa! I got this thing painting pixels in no time
+Holy bejeebers this tutorial is ON POINT.
+YAY! We have a left moving "marquee" of smiles
+Vim keys. YES PLEASE.
--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ impl GameState for State {
     fn tick(&mut self, ctx : &mut Rltk) {
         ctx.cls();
 
+        player_input(self, ctx);
         self.run_systems();
 
         let positions = self.ecs.read_storage::<Position>();
@@ -57,6 +58,40 @@ struct Renderable {
 #[derive(Component)]
 struct LeftMover {}
 
+#[derive(Component, Debug)]
+struct Player {}
+
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        pos.x = min(79, max(0, pos.x + delta_x));
+        pos.y = min(49, max(0, pos.y + delta_y));
+    }
+}
+
+fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    match ctx.key {
+        None => {} // No key event detected this tick
+        Some(key) => match key {
+            // Arrow key handling
+            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+
+            // And their vim counterparts
+            VirtualKeyCode::H => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::J => try_move_player(0, 1, &mut gs.ecs),
+            VirtualKeyCode::K => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::L => try_move_player(1, 0, &mut gs.ecs),
+
+            _ => {} // And the "any other key" catch all
+        }
+    }
+}
+
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
     let context = RltkBuilder::simple80x50()
@@ -68,11 +103,13 @@ fn main() -> rltk::BError {
 
     // Registration
     gs.ecs.register::<LeftMover>();
+    gs.ecs.register::<Player>();
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
 
-    // Entity Building
-    gs.ecs
+    // Entities
+
+    gs.ecs // Player
         .create_entity()
         .with(Position { x: 42, y: 42 })
         .with(Renderable {
@@ -80,9 +117,10 @@ fn main() -> rltk::BError {
             fg: RGB::named(rltk::YELLOW),
             bg: RGB::named(rltk::BLACK),
         }) 
+        .with(Player{})
         .build();
 
-    for i in 0..20 {
+    for i in 0..20 { // Dancing Smiles
         gs.ecs
         .create_entity()
         .with(Position {x: i * 4, y: 20})
